1b3c6a2bfb30ca3a02c4ca46a08726486729d388
Daniel Fesalbon <daniel.fesalbon@ibm.com>
2023-01-11 15:55:19 +0800

    add dot

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get('/')
 def hello_world():
-    return "hello world from devops"
+    return "hello world from devops.."
 
 #if __name__ == "__main__":
 #    uvicorn.run('main:app', port=8000, reload=False)
